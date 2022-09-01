5e40daa7cace2138fb58e57be5751d7bb27886c2
Barla6 <barlaliko62@gmail.com>
2022-09-01 14:59:26 +0300

    change gradle to use lib as module in server

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,24 +3,26 @@ val kotlin_version = "1.5.20"
 val logback_version: String by project
 
 plugins {
-    application
-    kotlin("jvm") version "1.5.20"
+//    application
+//    kotlin("jvm") version "1.5.20"
+    id("checkers.kotlin-application-conventions")
 }
 
-group = "com"
-version = "0.0.1"
+//group = "com"
+//version = "0.0.1"
 application {
-    mainClass.set("com.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    mainClass.set("checkers.CheckersServer.ApplicationKt")
+//    mainClass.set("com.ApplicationKt")
+//    val isDevelopment: Boolean = project.ext.has("development")
+//    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
-
-repositories {
-    mavenCentral()
-}
+//
+//repositories {
+//    mavenCentral()
+//}
 
 dependencies {
+    implementation(project(":CheckersLib"))
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
